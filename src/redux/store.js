7db554c234eaabf46e8d6f44c6f046a422eb7f21
David Skoda <dskoda1@binghamton.edu>
2018-07-01 21:46:07 -0400

    remove dev tools and service worker code

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,4 +1,4 @@
-import { createStore, applyMiddleware, compose } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import createSagaMiddleware from "redux-saga";
 
 import rootReducer from "./reducers";
@@ -8,14 +8,14 @@ import rootSaga from "./sagas";
 const sagaMiddleware = createSagaMiddleware();
 
 // dev tools middleware
-const reduxDevTools =
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
+// const reduxDevTools =
+//   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__();
 
 // create a redux store with our reducer above and middleware
 let store = createStore(
   rootReducer,
-//   applyMiddleware(sagaMiddleware)
-  compose(applyMiddleware(sagaMiddleware), reduxDevTools)
+  applyMiddleware(sagaMiddleware)
+  // compose(applyMiddleware(sagaMiddleware), reduxDevTools)
 );
 
 // run the saga
